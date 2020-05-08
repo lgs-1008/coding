60c5b813abee84d5931465c310db230b4786aefb
gyusun <gedit@kakao.com>
2020-05-08 23:22:33 +0900

    5/8

--- makebignum.py
+++ makebignum.py
@@ -1,20 +1,21 @@
-number = "4177252841"
+number = "44444444"
 k = 4
 
 def solution(number, k):
-    check = list(number)
-    j=0
-    i=1
-    while j!=k:
-        if check[i-1]>check[i]:
-            del check[i]
-            j+=1
-        elif check[i-1]<check[i]:
-            del check[i-1]
-            j+=1
-        else:
-            i+=2
-    return "".join(check)
+    stack = []
+    for i,j in enumerate(number):
+        while stack and stack[-1]<j and k>0:
+            del stack[-1]
+            k-=1
+        if k==0:
+            stack+=number[i:]
+            break
+
+        stack.append(j)
+
+    if k>0:
+        stack = stack[:-k]
+    return "".join(stack)
 
 
 
